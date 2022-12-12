4e8f05a49588cd98e6a36a90055edf27ac9b5584
VSAC <846366712@qq.com>
2022-12-12 13:23:57 +0800

    no message

---
+++ AppCenterSwiftLintExample/AppCenterSwiftLintExample/AppCenterSwiftLintExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCenterSwiftLintExampleApp.swift
+//  AppCenterSwiftLintExample
+//
+//  Created by MacBook on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppCenterSwiftLintExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
